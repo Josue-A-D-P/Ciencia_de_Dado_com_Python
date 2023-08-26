4b0a7cf86ba4682ed807491ce0c09934d4b80fd0
Josue <josueporto59@gmail.com>
2023-08-26 17:47:41 -0300

    Estruturas de repetição

---
+++ Introdução_a_ciências_de_dados(Python).py/estrutura_de_repeticao_break.py
@@ -0,0 +1,14 @@
+
+while True:
+    numero = int(input('informe um numero: '))
+
+    if numero == 10:
+        break
+    print(numero)
+
+for numero in range(11):
+    if numero == 5:
+        continue
+    print(numero)
+
+       
